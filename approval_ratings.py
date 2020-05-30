a8011391e8ce26f5ec02ec2923a719294c6aca73
chetkruw <chetkr@uw.edu>
2020-05-29 22:59:46 -0700

    Chet Push 2

--- approval_ratings.py
+++ approval_ratings.py
@@ -1,12 +1,15 @@
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
 import spacy
+import matplotlib.pyplot as plt
+
+sns.set()
 
 def organize_ratings(ratings):
     ratings = ratings[['subgroup', 'grade', 'samplesize', 'approve', 'disapprove']]
     ratings = ratings.sort_index()
 
-    # Only Care about polls that scored well and dealt with Adults
+    # Only care about polls that scored well and dealt with Adults
     approved_grades = ['A+', 'A', 'A-', 'A/B', 'B+', 'B', 'B-', 'B/C']
     isQuality = ratings['grade'].isin(approved_grades)
     isAdults = ratings['subgroup'] == 'All polls'
@@ -16,7 +19,7 @@ def organize_ratings(ratings):
     ratings['weighted_approve'] = ratings['samplesize'] * ratings['approve']
     ratings['weighted_disapprove'] = ratings['samplesize'] * ratings['disapprove']
 
-    ratings = ratings.resample('D').sum()
+    ratings = ratings.resample('W').sum()
     ratings['approve'] = ratings['weighted_approve'] / ratings['samplesize']
     ratings['disapprove'] = ratings['weighted_disapprove'] / ratings['samplesize']
 
@@ -27,11 +30,38 @@ def organize_tweets(tweets):
     tweets['count'] = 1
     tweets = tweets[['retweets', 'favorites', 'count']]
 
-    tweets = tweets.resample('D').sum()
+    tweets = tweets.resample('W').sum()
     tweets['retweet_avg'] = tweets['retweets'] / tweets['count']
     tweets['fav_avg'] = tweets['retweets'] / tweets['count']
 
     return tweets.loc['2017-01-20':'2020-4-15']
+
+
+def create_plots(approval_ratings):
+    approval_ratings = approval_ratings.resample('W').mean()
+
+    fig, [[ax1, ax2, ax3], [ax4, ax5, ax6]] = plt.subplots(nrows=2, ncols=3, figsize=(20,15))
+
+    sns.regplot(x='approve', y='count', data=approval_ratings, ax=ax1)
+    ax1.set(xlabel='Avg Approval Rating (Week)', ylabel='Weekly Tweet Count', title='Approval Ratings per Week vs. Weekly Tweet Count')
+
+    sns.regplot(x='approve', y='fav_avg', data=approval_ratings, ax=ax2)
+    ax2.set(xlabel='Avg Approval Rating (Week)', ylabel='Weekly Tweet Favorites', title='Approval Ratings per Week vs. Weekly Tweet Favorites')
+
+    sns.regplot(x='approve', y='retweet_avg', data=approval_ratings, ax=ax3)
+    ax3.set(xlabel='Avg Approval Rating (Week)', ylabel='Weekly Tweet Retweets', title='Approval Ratings per Week vs. Weekly Tweet Retweets')
+
+    sns.regplot(x='disapprove', y='count', data=approval_ratings, ax=ax4)
+    ax4.set(xlabel='Avg Disapproval Rating (Week)', ylabel='Weekly Tweet Count', title='Disapproval Ratings per Week vs. Weekly Tweet Count')
+
+    sns.regplot(x='disapprove', y='fav_avg', data=approval_ratings, ax=ax5)
+    ax5.set(xlabel='Avg Disapproval Rating (Week)', ylabel='Weekly Tweet Favorites', title='Disapproval Ratings per Week vs. Weekly Tweet Favorites')
+
+    sns.regplot(x='disapprove', y='retweet_avg', data=approval_ratings, ax=ax6)
+    ax6.set(xlabel='Avg Disapproval Rating (Week)', ylabel='Weekly Tweet Retweets', title='Disapproval Ratings per Week vs. Weekly Tweet Retweets')
+
+    fig.savefig('test.png')
+
 
 def main():
     print('cool')
@@ -44,10 +74,13 @@ def main():
                            index_col='enddate', parse_dates=True)
 
     ratings = organize_ratings(ratings)
-    print(ratings)
 
     tweets = organize_tweets(tweets)
-    print(tweets)
+
+    approval_ratings = tweets.join(ratings)
+
+    sns.set_context("paper")
+    create_plots(approval_ratings)
 
 if __name__ == '__main__':
     main()
